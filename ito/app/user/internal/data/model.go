62e3d96d8ed83fe4672355a4c7a530980a1d9975
lanjinghexuan <1258321826@qq.com>
2025-07-18 09:33:26 +0800

    区分前后台

--- ito/app/user/internal/data/model.go
+++
@@ -1,7 +0,0 @@
-package data
-
-type Users struct {
-	Id     int32  `gorm:"column:id;type:int;primaryKey;not null;" json:"id"`
-	Name   string `gorm:"column:name;type:varchar(50);default:NULL;" json:"name"`
-	Mobile string `gorm:"column:mobile;type:char(11);default:NULL;" json:"mobile"`
-}
